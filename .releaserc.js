f4603334e7cd9c08e952f80713450a1b2154e939
MarcinOstrowskiB3D <88039166+MarcinOstrowskiB3D@users.noreply.github.com>
2025-05-20 14:31:16 +0200

    fix(Docs): Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,30 +1,27 @@
-{
-  "branches": [
-    { "name": "master" }
-  ], 
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
-      "@semantic-release/changelog",
+      '@semantic-release/changelog',
       {
-        "changelogFile": "CHANGELOG.md",
-        "changelogTitle": "# Changelog\n\nAll notable changes to this project will be documented in this file."
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file.'
       }
     ],
     [
-      "semantic-release-github-pullrequest",
+      'semantic-release-github-pullrequest',
       {
-        "title": "chore(release): ${nextRelease.version}",
-        "message": "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
-        "base": "master",
-        "branch": (context) => {
+        title: 'chore(release): ${nextRelease.version}',
+        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+        base: 'master',
+        branch: (context) => {
           const branch = `release-pr-v${context.nextRelease.version.replace(/\./g, '-')}`;
           console.log('[debug] Using branch:', branch);
           return branch;
-        },
-        "sign": true
+        }
       }
-    ],
+    ]
   ]
-}
+};
